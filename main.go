ee63fbb9e9f2ed1b60685a3c1383b8142d85e867
Raphaël Pinson <raphael.pinson@camptocamp.com>
2017-07-19 22:19:35 +0200

    Add history to API

--- main.go
+++ main.go
@@ -76,11 +76,32 @@ func state(w http.ResponseWriter, r *http.Request) {
 	io.WriteString(w, string(content))
 }
 
+func history(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	st := strings.TrimPrefix(r.URL.Path, "/api/history/")
+	result, err := svc.ListObjectVersions(&s3.ListObjectVersionsInput{
+		Bucket: aws.String(bucket),
+		Prefix: aws.String(st),
+	})
+	if err != nil {
+		errObj := make(map[string]string)
+		errObj["error"] = fmt.Sprintf("State file history not found: %v", st)
+		errObj["details"] = fmt.Sprintf("%v", err)
+		j, _ := json.Marshal(errObj)
+		io.WriteString(w, string(j))
+		return
+	}
+
+	j, _ := json.Marshal(result.Versions)
+	io.WriteString(w, string(j))
+}
+
 func main() {
 	http.HandleFunc("/", idx)
 	staticFs := http.FileServer(http.Dir("static"))
 	http.Handle("/static/", http.StripPrefix("/static", staticFs))
 	http.HandleFunc("/api/states", states)
 	http.HandleFunc("/api/state/", state)
+	http.HandleFunc("/api/history/", history)
 	http.ListenAndServe(":80", nil)
 }
