camptocamp/terraboard

commit 885a4215615bd747ee09cd46a7e136b10f476640

Remove index for attribute value (#39)

types/db.go
@@ -53,5 +53,5 @@ type Attribute struct {
 	ID         uint          `sql:"AUTO_INCREMENT" gorm:"primary_key" json:"-"`
 	ResourceID sql.NullInt64 `gorm:"index" json:"-"`
 	Key        string        `gorm:"index" json:"key"`
-	Value      string        `gorm:"index" json:"value"`
+	Value      string
 }

commit aa4fcc7a1fb2cacc22a9f56f9fd7ca7a4d1742ba

Fix json field for value
This was broken by 885a4215615bd747ee09cd46a7e136b10f476640

types/db.go
@@ -53,5 +53,5 @@ type Attribute struct {
 	ID         uint          `sql:"AUTO_INCREMENT" gorm:"primary_key" json:"-"`
 	ResourceID sql.NullInt64 `gorm:"index" json:"-"`
 	Key        string        `gorm:"index" json:"key"`
-	Value      string
+	Value      string        `json:"value"`
 }