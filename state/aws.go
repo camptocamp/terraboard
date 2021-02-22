fb684499c57e87ed216e3d1228ed36859c6b57ff
Aleix Murtra <alemurbcn@gmail.com>
2021-02-22 09:21:19 +0100

    Implement AWS EXTERNAL ID feature

--- state/aws.go
+++ state/aws.go
@@ -35,7 +35,9 @@ func NewAWS(c *config.Config) AWS {
 
 	if len(c.AWS.APPRoleArn) > 0 {
 		log.Debugf("Using %s role", c.AWS.APPRoleArn)
-		creds := stscreds.NewCredentials(sess, c.AWS.APPRoleArn)
+		creds := stscreds.NewCredentials(sess, c.AWS.APPRoleArn, func(p *stscreds.AssumeRoleProvider) {
+			p.ExternalID = aws_sdk.String(c.AWS.ExternalID)
+		})
 		awsConfig.WithCredentials(creds)
 	}
 
